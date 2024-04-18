8fa629d62189bcb7864b0c7303bf637de9d9c421
Priyanka Dhanabal <priyanka.dhanabal96@gmail.com>
2024-04-18 10:44:18 +0000

    added function and testing

--- scripts/game.js
+++ scripts/game.js
@@ -1,20 +1,48 @@
 let game = {
-    score: 0,
     currentGame: [],
     playerMoves: [],
-    choices: ["button1","button2","button3","button4"]
-}
+    score: 0,
+    turnNumber: 0,
+    choices: ["button1", "button2", "button3", "button4"]
+};
 
-function newGame(){
-    game.score = 0;
-    game.playerMoves = [];
+function newGame() {
     game.currentGame = [];
+    game.playerMoves = [];
+    game.score = 0;
     showScore();
+    addTurn();
 }
 
-function showScore(){
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
+}
+
+function showTurns() {
+    game.turnNumber = 0;
+    let turns = setInterval(function () {
+        lightsOn(game.currentGame[game.turnNumber]);
+        game.turnNumber++;
+        if (game.turnNumber >= game.currentGame.length) {
+            clearInterval(turns);
+        }
+    }, 800);
+}
+
+function lightsOn(circ) {
+    const button = document.getElementById(circ);
+    button.classList.add(`${circ}light`);
+    setTimeout(function () {
+      button.classList.remove(`${circ}light`);
+    }, 400);
+  }
+
+function showScore() {
     document.getElementById("score").innerText = game.score;
 }
 
+newGame();
 
-module.exports = { game, newGame, showScore };
+module.exports = { game, newGame, showScore, addTurn, lightsOn, showTurns };
